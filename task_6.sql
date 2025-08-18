80ce1649f01d64b940247f58cff65c8080c2b9b0
Steve Adansi <stevorica1@gmail.com>
2025-08-18 20:22:16 +0000

    updated spacing in text

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,8 @@
 USE alx_book_store
 -- Insert multiple rows into Customers table
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
